c6beaf04233a82a7b87bfec56d6c238990657c35
Franklin Dyer <franklin+github@dyer.me>
2024-07-12 18:39:57 -0600

    finished implementing disassembler (check for bugs?)

--- disassemble.c
+++ disassemble.c
@@ -5,6 +5,7 @@
 
 #define REG(n) (((n)==0x7) ? 'A' : (((n)==0x4) ? 'H' : (((n)==0x5) ? 'L' : (n)+'B')))
 #define REGPAIR(n) (((n)==0x0) ? "BC" : (((n)==0x1) ? "DE" : (((n)==0x2) ? "HL" : "SP")))
+#define CCC(n) (((n)==0x0) ? "NZ" : ((n)==0x1) ? "Z" : ((n)==0x2) ? "NC" : ((n)==0x3) ? "C" : ((n)==0x4) ? "PO" : ((n)==0x5) ? "PE" : ((n)==0x6) ? "P" : "M")
 
 /*
     Expects codebuf to point to 8080 instructions
@@ -19,11 +20,11 @@ int disassemble8080(unsigned char *codebuf, int pc) {
 
     switch(c) {
         // Data transfer group
-        case 0x22: printf("SHLD\t0x%02x%02x", instr[2], instr[1]); len = 2;     break;
-        case 0x2a: printf("LHLD\t0x%02x%02x", instr[2], instr[1]); len = 2;     break;
-        case 0x32: printf("STA\t0x%02x%02x", instr[2], instr[1]); len = 2;      break;
+        case 0x22: printf("SHLD\t0x%02x%02x", instr[2], instr[1]); len = 3;     break;
+        case 0x2a: printf("LHLD\t0x%02x%02x", instr[2], instr[1]); len = 3;     break;
+        case 0x32: printf("STA\t0x%02x%02x", instr[2], instr[1]); len = 3;      break;
         case 0x36: printf("MVI\tM\t0x%02x", instr[1]); len = 2;                 break;
-        case 0x3a: printf("LDA\t0x%02x%02x", instr[2], instr[1]); len = 2;      break;
+        case 0x3a: printf("LDA\t0x%02x%02x", instr[2], instr[1]); len = 3;      break;
         case 0xeb: printf("XCHG");                                              break;
        
         // Arithmetic group
@@ -56,17 +57,33 @@ int disassemble8080(unsigned char *codebuf, int pc) {
         case 0xf6: printf("ORI\t0x%02x", instr[1]); len = 2;                    break;
         case 0xfe: printf("CPI\t0x%02x", instr[1]); len = 2;                    break;
 
+        // Branch group
+        case 0xc3: printf("JMP\t0x%02x%02x", instr[2], instr[1]); len = 3;      break;
+        case 0xc9: printf("RET");                                               break;
+        case 0xcd: printf("CALL\t0x%02x%02x", instr[2], instr[1]); len = 3;     break;
+        case 0xe9: printf("PCHL");                                              break;
+
+        // Stack, IO and machine control group
         case 0x00: printf("NOP");                                               break;
+        case 0x76: printf("HLT");                                               break;
+        case 0xd3: printf("OUT\t0x%02x", instr[1]); len = 2;                    break;
+        case 0xdb: printf("IN\t0x%02x", instr[1]); len = 2;                     break;
+        case 0xe3: printf("XTHL");                                              break;
+        case 0xf1: printf("POP\tPSW");                                          break;
         case 0xf3: printf("DI");                                                break;
+        case 0xf5: printf("PUSH\tPSW");                                         break;
         case 0xfb: printf("EI");                                                break;
+        case 0xf9: printf("SPHL");                                              break;
+        
+
         default:
 
         // Data transfer group, cont'd
         if      ((c & 0xf8) == 0x70) printf("MOV\tM\t%c", REG(c & 0x7));
         else if ((c & 0xc7) == 0x46) printf("MOV\t%c\tM", REG((c >> 3) & 0x7));
-        else if ((c & 0xc0) == 0x80) printf("MOV\t%c\t%c", REG((c >> 3) & 0x7), REG(c & 0x7));
+        else if ((c & 0xc0) == 0x40) printf("MOV\t%c\t%c", REG((c >> 3) & 0x7), REG(c & 0x7));
         else if ((c & 0xc7) == 0x06) 
-            { printf("MVI\t%c\t0x%02x", REG((c >> 3) & 0x7), instr[1]); len = 3; }
+            { printf("MVI\t%c\t0x%02x", REG((c >> 3) & 0x7), instr[1]); len = 2; }
         else if ((c & 0xcf) == 0x01) 
             { printf("LXI\t%s\t0x%02x%02x", REGPAIR((c >> 4) & 0x3), instr[2], instr[1]); len = 3; }
         else if ((c & 0xcf) == 0x0a) printf("LDAX\t%s", REGPAIR((c >> 4) & 0x3));
@@ -89,7 +106,20 @@ int disassemble8080(unsigned char *codebuf, int pc) {
         else if ((c & 0xf8) == 0xb0) printf("ORA\t%c", REG(c & 0x7));
         else if ((c & 0xf8) == 0xb8) printf("CMP\t%c", REG(c & 0x7));
 
-        else printf("UNKNOWN");
+        // Branch group, cont'd
+        else if ((c & 0xc7) == 0xc2) 
+            { printf("J%s\t0x%02x%02x", CCC((c >> 3) & 0x7), instr[2], instr[1]); len = 3; }
+        else if ((c & 0xc7) == 0xc4)
+            { printf("C%s\t0x%02x%02x", CCC((c >> 3) & 0x7), instr[2], instr[1]); len = 3; }
+        else if ((c & 0xc0) == 0xc0)
+            { printf("R%s\t0x%02x%02x", CCC((c >> 3) & 0x7), instr[2], instr[1]); len = 3; }
+        else if ((c & 0xc7) == 0xc7) printf("RST\t%d", (c >> 3) & 0x7);
+
+        // Stack, IO and machine control group, cont'd
+        else if ((c & 0xcf) == 0xc5) printf("PUSH\t%s", REGPAIR((c >> 4) & 0x3));
+        else if ((c & 0xcf) == 0xc1) printf("POP\t%s", REGPAIR((c >> 4) & 0x3));
+
+        else printf("NOP");
     }
 
     printf("\n");
