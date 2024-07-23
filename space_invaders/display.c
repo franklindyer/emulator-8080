6e7288695ccb5fd1058e6954470e52dc970d8399
Franklin Dyer <franklin+github@dyer.me>
2024-07-22 22:38:44 -0600

    abstracted screen drawing functions to allow for arbitrary color maps

--- space_invaders/display.c
+++ space_invaders/display.c
@@ -35,9 +35,18 @@ space_invaders_display init_space_invaders_display(unsigned char* vidmem) {
     return disp;
 }
 
+rgb_pixel space_invaders_color_map(int x, int y, int on) {
+    rgb_pixel pix = {
+        (255 * x * on) / (2 * 256),
+        (255 * y * on) / (2 * 224),
+        (255 * (2*256 - x) * on) / (2 * 256)
+    };
+    return pix;
+}
+
 void update_space_invaders_display(space_invaders_display* disp) {
     int i;
-    draw_pixel_screen_rotated(disp->image_surface, disp->bitmap, 224, 256);
+    draw_pixel_screen_rotated(disp->image_surface, disp->bitmap, 224, 256, &space_invaders_color_map);
     SDL_BlitSurface(disp->image_surface, NULL, disp->window_surface, NULL);
     SDL_UpdateWindowSurface(disp->window);
 }
